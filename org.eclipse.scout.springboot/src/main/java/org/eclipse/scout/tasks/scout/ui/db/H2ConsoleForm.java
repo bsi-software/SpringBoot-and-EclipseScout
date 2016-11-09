6b9a156ada7af2532b6ec3f78121261935ca7bcf
Michael Schmuki <michael.schmuki@bsi-software.com>
2016-11-09 18:57:33 +0100

    Added H2 to AdminOutline (POC)

---
+++ org.eclipse.scout.springboot/src/main/java/org/eclipse/scout/tasks/scout/ui/db/H2ConsoleForm.java
@@ -0,0 +1,65 @@
+package org.eclipse.scout.tasks.scout.ui.db;
+
+import org.eclipse.scout.rt.client.ui.form.AbstractForm;
+import org.eclipse.scout.rt.client.ui.form.fields.browserfield.AbstractBrowserField;
+import org.eclipse.scout.rt.client.ui.form.fields.groupbox.AbstractGroupBox;
+import org.eclipse.scout.rt.platform.Bean;
+import org.eclipse.scout.rt.platform.Order;
+
+@Bean
+public class H2ConsoleForm extends AbstractForm {
+
+  @Override
+  protected int getConfiguredDisplayHint() {
+    return DISPLAY_HINT_VIEW;
+  }
+
+  @Order(1000)
+  public class MainBox extends AbstractGroupBox {
+
+    @Override
+    protected boolean getConfiguredLabelVisible() {
+      return false;
+    }
+
+    @Override
+    protected boolean getConfiguredStatusVisible() {
+      return false;
+    }
+
+    @Order(1000)
+    public class BrowserField extends AbstractBrowserField {
+
+      @Override
+      protected double getConfiguredGridWeightY() {
+        return 1;
+      }
+
+      @Override
+      protected boolean getConfiguredLabelVisible() {
+        return false;
+      }
+
+      @Override
+      protected boolean getConfiguredStatusVisible() {
+        return false;
+      }
+
+      @Override
+      protected boolean getConfiguredSandboxEnabled() {
+        return false;
+      }
+
+      @Override
+      protected boolean getConfiguredScrollBarEnabled() {
+        return true;
+      }
+
+      @Override
+      protected void execInitField() {
+        setLocation("http://localhost:8080/h2-console");
+      }
+    }
+  }
+
+}
